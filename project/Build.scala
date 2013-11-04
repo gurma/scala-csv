62c3c9b27c273267450f057da4f2113bfc91e76a
Markus Rentto <mrentt@gmail.com>
2013-11-04 16:00:09 +0200

    Added osgi and eclipse support

--- project/Build.scala
+++ project/Build.scala
@@ -1,42 +1,47 @@
 import sbt._
 import Keys._
+import com.typesafe.sbt.osgi.OsgiKeys
+import com.typesafe.sbt.osgi._
 
 object ScalaCSVProject extends Build {
 
-  lazy val root = Project (
+  lazy val osgisettings = Seq(
+    OsgiKeys.exportPackage := Seq("com.github.tototoshi.csv"),
+    OsgiKeys.additionalHeaders := Map("Include-Resource" -> ""))
+
+  lazy val root = Project(
     id = "scala-csv",
-    base = file ("."),
-    settings = Defaults.defaultSettings ++ Seq (
+    base = file("."),
+    settings = Defaults.defaultSettings ++ Seq(
       name := "scala-csv",
       version := "1.0.0-SNAPSHOT",
       scalaVersion := "2.10.0",
       crossScalaVersions := Seq("2.10.0", "2.9.1", "2.9.2", "2.9.3"),
       organization := "com.github.tototoshi",
       libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest" % "1.9.1" % "test"
-      ),
+        "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+        "org.osgi" % "org.osgi.core" % "4.2.0"),
       scalacOptions <<= scalaVersion.map { sv =>
         if (sv.startsWith("2.10")) {
           Seq(
             "-deprecation",
-            "-language:_"
-          )
+            "-language:_")
         } else {
           Seq("-deprecation")
         }
       },
+      
       publishMavenStyle := true,
       publishTo <<= version { (v: String) =>
         val nexus = "https://oss.sonatype.org/"
         if (v.trim.endsWith("SNAPSHOT"))
           Some("snapshots" at nexus + "content/repositories/snapshots")
         else
-          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
       },
       publishArtifact in Test := false,
-      pomExtra := _pomExtra
-    )
-  )
+      pomExtra := _pomExtra) ++
+      com.typesafe.sbt.osgi.SbtOsgi.osgiSettings ++ osgisettings)
 
   val _pomExtra =
     <url>http://github.com/tototoshi/scala-csv</url>
---
+++ project/plugins.sbt
@@ -0,0 +1,5 @@
+addSbtPlugin("com.typesafe.sbteclipse" %% "sbteclipse-plugin" % "2.3.0")
+
+addSbtPlugin("com.typesafe.sbt" %% "sbt-osgi" % "0.6.0")
+
+resolvers += "sonatype-public" at "https://oss.sonatype.org/?content/repositories/public"
